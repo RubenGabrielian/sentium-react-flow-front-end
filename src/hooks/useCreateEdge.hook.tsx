410c9dcb4931961ea734b381f4fa3ae9a24ad009
Ruben Gabrielian <gabrielianruben@gmail.com>
2023-09-02 20:58:17 +0400

    fix ts issue:

--- src/hooks/useCreateEdge.hook.tsx
+++ src/hooks/useCreateEdge.hook.tsx
@@ -1,17 +1,18 @@
 import { IEdge } from "@/entities/edges";
 import { edgesApi } from "@/services/edges";
+import { AxiosResponse } from "axios";
 import { useMutation, useQueryClient } from "react-query";
 
 export const useCreateEdge = (
     onSuccess = (data: IEdge) => { },
-    onError = (e) => { },
+    onError = (e: AxiosResponse) => { },
 ) => {
     const client = useQueryClient();
     const createEdge = useMutation(edgesApi.createEdge, {
         onSuccess: (data) => {
             onSuccess(data);
         },
-        onError: (e) => {
+        onError: (e: AxiosResponse) => {
             onError(e);
         },
     });
